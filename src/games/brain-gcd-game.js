5576c23e62a8a347beec3d32c930c38dbcbad45a
Dmitry Kabanov <dkabanov6@gmail.com>
2018-04-21 06:59:04 +0500

    add game for finding the greatest common divisor

---
+++ src/games/brain-gcd-game.js
@@ -0,0 +1,36 @@
+import runGameEngine from '../game-engine';
+import { getRandomNumber } from '../numbers';
+
+const FindGreatestCommonDivisor = (numberOne, numberTwo) => {
+  const iter = (acc) => {
+    if (numberOne % acc === 0 && numberTwo % acc === 0) {
+      return acc;
+    }
+
+    return iter(acc - 1);
+  };
+
+  const largestNumber = numberOne >= numberTwo ? numberOne : numberTwo;
+  return iter(largestNumber);
+};
+
+const task = 'Find the greatest common divisor of given numbers.';
+
+const generateInformationForGcdGame = () => {
+  const numberOne = getRandomNumber(1, 20);
+  const numberTwo = getRandomNumber(1, 20);
+
+  const question = `${numberOne} ${numberTwo}`;
+  const correctAnswer = `${FindGreatestCommonDivisor(numberOne, numberTwo)}`;
+
+  return {
+    question,
+    correctAnswer,
+  };
+};
+
+const runGcd = () => {
+  runGameEngine(task, generateInformationForGcdGame);
+};
+
+export default runGcd;

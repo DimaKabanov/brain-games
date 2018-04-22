b9e83115917e2bd11f3c026874d1d2a4d96e366f
Dmitry Kabanov <dkabanov6@gmail.com>
2018-04-22 07:38:16 +0500

    add new game brain-progression

---
+++ src/bin/brain-progression.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import runProgression from '../games/brain-progression-game';
+
+runProgression();
---
+++ src/games/brain-progression-game.js
@@ -0,0 +1,65 @@
+import runGameEngine from '../game-engine';
+import { getRandomNumber, compareNumbers } from '../numbers';
+
+const createProgression = (start, step, length) => {
+  const iter = (counter, previousNumber, acc) => {
+    if (counter === 0) {
+      return acc;
+    }
+    const nextNumber = previousNumber + step;
+    return iter(counter - 1, nextNumber, acc.concat(nextNumber));
+  };
+
+  return iter(length, start, [start]);
+};
+
+const createCorrectAnswer = (progression, lengthAnswer) => {
+  const iter = (counter, acc) => {
+    if (counter === 0) {
+      return acc;
+    }
+    const randomIndex = getRandomNumber(1, 3);
+    const itemProgression = progression[randomIndex];
+    const newCounter = acc.includes(itemProgression) ? counter : counter - 1;
+    const newAcc = acc.includes(itemProgression) ? acc : acc.concat(itemProgression);
+    return iter(newCounter, newAcc);
+  };
+
+  return iter(lengthAnswer, []);
+};
+
+const hideItemsProgression = (progression, hiddenItems) => {
+  const result = progression.reduce((acc, item) => {
+    if (hiddenItems.includes(item)) {
+      return acc.concat('..');
+    }
+    return acc.concat(item);
+  }, []);
+
+  return result;
+};
+
+const task = 'What number is missing in this progression?';
+
+const createInformationForProgressionGame = () => {
+  const startNumber = getRandomNumber(1, 10);
+  const step = getRandomNumber(1, 10);
+  const lengthProgression = 10;
+  const lengthAnswer = getRandomNumber(1, 3);
+  const currentProgression = createProgression(startNumber, step, lengthProgression);
+  const currentCorrectAnswer = createCorrectAnswer(currentProgression, lengthAnswer);
+
+  const question = hideItemsProgression(currentProgression, currentCorrectAnswer).join(' ');
+  const correctAnswer = currentCorrectAnswer.sort(compareNumbers).join(' ');
+
+  return {
+    question,
+    correctAnswer,
+  };
+};
+
+const runProgression = () => {
+  runGameEngine(task, createInformationForProgressionGame);
+};
+
+export default runProgression;

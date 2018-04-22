d4d20a0aadb897daf5d945d091b49e2ce1b58e84
Dmitry Kabanov <dkabanov6@gmail.com>
2018-04-22 15:18:51 +0500

    rewrote the function findGreatestCommonDivisor using Euclid's algorithm

--- src/games/brain-gcd-game.js
+++ src/games/brain-gcd-game.js
@@ -2,23 +2,30 @@ import runGameEngine from '../game-engine';
 import { getRandomNumber } from '../numbers';
 
 const FindGreatestCommonDivisor = (numberOne, numberTwo) => {
-  const iter = (acc) => {
-    if (numberOne % acc === 0 && numberTwo % acc === 0) {
-      return acc;
+  if (numberOne === numberTwo) {
+    return numberOne;
+  }
+
+  const iter = (large, small) => {
+    const remainderOfDivision = large % small;
+
+    if (remainderOfDivision === 0) {
+      return small;
     }
 
-    return iter(acc - 1);
+    return iter(small, remainderOfDivision);
   };
 
-  const largestNumber = numberOne >= numberTwo ? numberOne : numberTwo;
-  return iter(largestNumber);
+  const largestNumber = Math.max(numberOne, numberTwo);
+  const smallestNumber = Math.min(numberOne, numberTwo);
+  return iter(largestNumber, smallestNumber);
 };
 
 const task = 'Find the greatest common divisor of given numbers.';
 
 const generateInformationForGcdGame = () => {
-  const numberOne = getRandomNumber(1, 20);
-  const numberTwo = getRandomNumber(1, 20);
+  const numberOne = getRandomNumber(1, 200);
+  const numberTwo = getRandomNumber(1, 200);
 
   const questionData = `${numberOne} ${numberTwo}`;
   const correctAnswer = `${FindGreatestCommonDivisor(numberOne, numberTwo)}`;
